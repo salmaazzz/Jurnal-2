d63da2065c74790c83c70b00c5eed739b8cd1f2e
salmaazzz <azizahsalma2304@gmail.com>
2023-09-21 12:19:54 +0700

    first commit

---
+++ hello.java
@@ -0,0 +1,7 @@
+public class hello {
+    public static void main(String[] args){
+        String arr[] = {"hello ","world"};
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+    }
+}
